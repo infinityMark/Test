1b5693d01fb57b3d0fd628c438b8ea5b01f085a8
infinityMark <109346958+infinityMark@users.noreply.github.com>
2025-03-22 08:55:00 +0800

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//
+//double hypoten(double x, double y)
+//{
+//	double z;
+//	z = sqrt(x * x + y * y);
+//	return z;
+//}
+//
+//int main() {
+//
+//	double side1, side2;
+//	cout << "Please enter the length of first side: ";
+//	cin >> side1;
+//	cout << "Please enter the length of second side: ";
+//	cin >> side2;
+//
+//	cout << "Hypotenuse of a " << side1 << " by " << side2 << " right triangle is " << hypoten(side1, side2);
+//
+//	return 0;
+//}
